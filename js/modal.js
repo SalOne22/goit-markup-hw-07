9b515c8473b1615a2da925030a136eaf89cb5792
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-01-29 01:03:22 +0100

    Finaled homework

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,21 @@
 ;(() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]')
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]")
   }
-
-  refs.openModalBtn.addEventListener('click', toggleModal)
-  refs.closeModalBtn.addEventListener('click', toggleModal)
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden')
+    const isMenuOpen =
+      refs.openModalBtn.getAttribute("aria-expanded") === "true" || false
+    refs.openModalBtn.setAttribute("aria-expanded", !isMenuOpen)
+    refs.modal.classList.toggle("is-hidden")
+    const scrollLockMethod = !isMenuOpen
+      ? "disableBodyScroll"
+      : "enableBodyScroll"
+    bodyScrollLock[scrollLockMethod](document.body)
   }
+
+  refs.openModalBtn.addEventListener("click", toggleModal)
+  refs.closeModalBtn.addEventListener("click", toggleModal)
 })()
