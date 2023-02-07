c62852186deb9938cf0532aa4218bae844fdc5b5
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-02-07 23:30:32 +0100

    Added filter function to portfolio

---
+++ js/filter.js
@@ -0,0 +1,13 @@
+function filterCards(e) {
+    const cards = document.querySelectorAll(".card");
+    let filter = e.target.dataset.filter;
+    if (filter === '*') {
+      cards.forEach(card => card.classList.remove('hidden'));
+    }  else {
+      cards.forEach(card => {
+        card.dataset.category === filter ? 
+        card.classList.remove('hidden') : 
+        card.classList.add('hidden');
+      });
+    };
+  };
