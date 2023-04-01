29f72d3b6d46fa500244d8577a28a7b332976cba
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-04-01 23:59:31 +0200

    Formatted JavaScript

--- js/modal.js
+++ js/modal.js
@@ -1,21 +1,21 @@
-;(() => {
+(() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]")
-  }
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
   function toggleModal() {
     const isMenuOpen =
-      refs.openModalBtn.getAttribute("aria-expanded") === "true" || false
-    refs.openModalBtn.setAttribute("aria-expanded", !isMenuOpen)
-    refs.modal.classList.toggle("is-hidden")
+      refs.openModalBtn.getAttribute('aria-expanded') === 'true' || false;
+    refs.openModalBtn.setAttribute('aria-expanded', !isMenuOpen);
+    refs.modal.classList.toggle('is-hidden');
     const scrollLockMethod = !isMenuOpen
-      ? "disableBodyScroll"
-      : "enableBodyScroll"
-    bodyScrollLock[scrollLockMethod](document.body)
+      ? 'disableBodyScroll'
+      : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);
   }
 
-  refs.openModalBtn.addEventListener("click", toggleModal)
-  refs.closeModalBtn.addEventListener("click", toggleModal)
-})()
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+})();
